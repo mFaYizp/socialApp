c4a8bc878f2b89dcab00f7e1d437000656cd20a0
mFaYizp <fayizfaz78@gmail.com>
2023-08-25 00:10:21 +0530

    moment(time)setted

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -9,7 +9,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import moment from "moment";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import DeleteIcon from "@material-ui/icons/Delete";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 
@@ -25,11 +25,11 @@ const Post = ({ post,setCurrentId }) => {
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">
-          {moment(post.createdAt).fromNow}
+          {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{ color: "white" }} size="small" onClick={() => {setCurrentId(post._id)}}>
+        <Button style={{ color: "white" }} size="small" onClick={() => setCurrentId(post._id)}>
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CircularProgress, Grid } from "@material-ui/core";
+import { fetchPosts } from "../../api";
 
 const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    fetchPosts(dispatch)
+  },[dispatch])
 
   console.log(posts);
   return !posts.length ? 
