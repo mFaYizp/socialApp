a57a6d20f4c152d8af419c6a1964b6380b392eaa
mFaYizp <fayizfaz78@gmail.com>
2023-08-26 22:42:59 +0530

    liking post

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -12,12 +12,12 @@ import moment from "moment";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import DeleteIcon from "@material-ui/icons/Delete";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
-import {useDispatch} from "react-redux"
-import {deletePost} from "../../../actions/posts"
+import { useDispatch } from "react-redux";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -49,17 +49,29 @@ const Post = ({ post, setCurrentId }) => {
         {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="body2" color="textSecondary" component="p">
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {
+            dispatch(likePost(post._id));
+          }}
+        >
           <ThumbUpAltIcon fontSize="small" />
-          Like
+          &nbsp; Like &nbsp;
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {dispatch(deletePost(post._id)) }}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {
+            dispatch(deletePost(post._id));
+          }}
+        >
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import postRouter from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-const MONGO_URL = "mongodb+srv://Fayiz:fa786678@cluster0.wzcsy5e.mongodb.net/memories?retryWrites=true&w=majority"
-const PORT = 4000;
-
+const PORT = 4000 || process.env.PORT;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,6 +17,6 @@ app.use(cors());
 app.use("/posts", postRouter);
 
 mongoose
-  .connect(MONGO_URL)
+  .connect(process.env.MONGO_URL)
   .then(() => app.listen(PORT, () => console.log(`server running on: ${PORT}`)))
   .catch((error) => console.log(error.message));
