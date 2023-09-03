be1bf33fe8d6b0814ccc7eb73d9ef0d7f8a87e87
mFaYizp <fayizfaz78@gmail.com>
2023-09-03 20:07:47 +0530

    like updated

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -11,9 +11,9 @@ import auth from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/", auth,createPost);
-router.patch("/:id", auth,updatePost);
-router.delete("/:id", auth,deletePost);
-router.patch("/:id/likePost", auth,likePost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
